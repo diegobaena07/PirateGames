09d906f42c54d9ecc2e2412d54885d3b6ed29384
Diego Baena <baena1907@gmail.com>
2023-11-01 17:31:36 -0500

    Modificaciones de pantalla

--- pirate_games/src/pages/PirateGames/components/Carousel/Carousel.tsx
+++ pirate_games/src/pages/PirateGames/components/Carousel/Carousel.tsx
@@ -1,114 +1,62 @@
-import { useEffect, useRef, useState } from "react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Carousel.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Imagen } from "../../../../interfaces/General";
 
-type imagenes = {
-  id: number;
-  imgUrl: string;
-};
-
-const noticias: Array<imagenes> = [
-  {
-    id: 1,
-    imgUrl: "./img/black banner.jpg",
-  },
-  {
-    id: 2,
-    imgUrl: "./img/DBZ Budokai Tenkaichi 4 banner.jpg",
-  },
-  {
-    id: 3,
-    imgUrl: "./img/Dios de la guerra 3 banner.jpg",
-  },
-  {
-    id: 4,
-    imgUrl: "./img/Efootball 2024 banner.jpg",
-  },
+const noticias: Array<Imagen> = [
+    {
+        id: 1,
+        imgUrl: "./img/black banner.jpg",
+    },
+    {
+        id: 2,
+        imgUrl: "./img/DBZ Budokai Tenkaichi 4 banner.jpg",
+    },
+    {
+        id: 3,
+        imgUrl: "./img/Dios de la guerra 3 banner.jpg",
+    },
+    {
+        id: 4,
+        imgUrl: "./img/Efootball 2024 banner.jpg",
+    },
 ];
 
 const Carousel = () => {
-  return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        loop={true}
-        allowTouchMove={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: true,
-        }}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true,
-        }}
-        navigation={false}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        {
-        noticias.map((item) => {
-          return (
-            <SwiperSlide className="">
-                <img className='imagen-noticia' src={item.imgUrl} />
-            </SwiperSlide>
-          );
-        })
-        }
-      </Swiper>
-    </>
-  );
-  // const listRef = useRef<HTMLUListElement | null>(null);
-  // const [currentIndex, setCurrentIndex] = useState(0);
-
-  // useEffect(() => {
-  //     const listNode = listRef.current;
-  //     const imgNode = listNode?.querySelectorAll("li > img")[currentIndex];
-  //     console.log(imgNode)
-
-  //     if (imgNode) {
-  //         imgNode.scrollIntoView({
-  //             behavior: "smooth"
-  //         });
-  //     }
-
-  // }, [currentIndex]);
-
-  // const goToSlide = (slideIndex: number) => {
-  //     setCurrentIndex(slideIndex);
-  // }
-
-  // return (
-  //     <div className="main-container">
-  //         <div className="slider-container">
-  //             <div className="container-images">
-  //                 <ul ref={listRef}>
-  //                     {
-  //                         noticias.map((item) => {
-  //                             return <li key={item.id}>
-  //                                 <img className='imagen-noticia' src={item.imgUrl} />
-  //                             </li>
-  //                         })
-  //                     }
-  //                 </ul>
-  //             </div>
-  //             <div className="dots-container">
-  //                 {
-  //                     noticias.map((_, idx) => (
-  //                         <div key={idx}
-  //                             className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
-  //                             onClick={() => goToSlide(idx)}>
-  //                             &#9865;
-  //                         </div>))
-  //                 }
-  //             </div>
-  //         </div>
-  //     </div >
-  // )
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                allowTouchMove={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: true,
+                }}
+                pagination={{
+                    clickable: true,
+                    dynamicBullets: true,
+                }}
+                navigation={false}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                {
+                    noticias.map((item) => {
+                        return (
+                            <SwiperSlide className="">
+                                <img className='imagen-noticia' src={item.imgUrl} />
+                            </SwiperSlide>
+                        );
+                    })
+                }
+            </Swiper>
+        </>
+    );
 };
 
 export { Carousel };
--- pirate_games/src/pages/PirateGames/pages/InicioPirateGames/InicioPirateGames.tsx
+++ pirate_games/src/pages/PirateGames/pages/InicioPirateGames/InicioPirateGames.tsx
@@ -1,81 +1,72 @@
+import { Juego } from "../../../../interfaces/General";
 import { Carousel } from "../../components/Carousel/Carousel";
 import { CaratulaJuego } from "../../components/CartaDeJuego/CaratulaJuego";
 import "./InicioPirateGames.css";
 import { FC } from "react"
 
+const juegos: Array<Juego> = [
+    {
+        id: 1,
+        imagen: "https://img.asmedia.epimg.net/resizer/lqsy7mJHPWIbj45aTSL7gwZY4U8=/360x0/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KJSGVNUE3ZJSFLZ5MUJVKHHILY.jpg",
+        nombre: "Grand Theft Auto: San Andreas",
+        region: "Ntfs-Pal",
+    },
+    {
+        id: 2,
+        imagen: "./img/Need for Speed Most Wanted.jpg",
+        nombre: "Need for Speed Most Wanted",
+        region: "Ntfs-Pal",
+    },
+    {
+        id: 3,
+        imagen: "./img/Naruto Ultimate Ninja 3.jpg",
+        nombre: "Naruto Ultimate Ninja 3",
+        region: "Ntfs-Pal",
+    },
+    {
+        id: 4,
+        imagen: "./img/PES 2020 3.0 FINAL.jpg",
+        nombre: "PES 2020 3.0 FINAL",
+        region: "Ntfs-Pal",
+    },
+    {
+        id: 5,
+        imagen: "./img/DBZ Budokai Tenkaichi 3.jpg",
+        nombre: "DBZ Budokai Tenkaichi 3",
+        region: "Ntfs-Pal",
+    },
+    {
+        id: 6,
+        imagen: "./img/Bully Canis Canem Edit.jpg",
+        nombre: "Bully Canis Canem Edit",
+        region: "Ntfs-Pal",
+    },
+];
 
 const InicioPirateGames: FC = () => {
     return (
         <>
             <div className="body-cartas">
                 <div className="back-search">
-                    <Carousel/>
+                    <Carousel />
                 </div>
                 <div className="back-cartas">
                     <div className="titulo-cartas">
                         <h3 className="titulo1">Juegos Agregados Recientemente</h3>
                     </div>
                     <div className="total-cartas">
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="https://img.asmedia.epimg.net/resizer/lqsy7mJHPWIbj45aTSL7gwZY4U8=/360x0/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KJSGVNUE3ZJSFLZ5MUJVKHHILY.jpg"
-                                nombre="Grand Theft Auto: San Andreas"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="./img/Need for Speed Most Wanted.jpg"
-                                nombre="Need for Speed Most Wanted"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="./img/Bully Canis Canem Edit.jpg"
-                                nombre="Bully Canis Canem Edit"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="./img/DBZ Budokai Tenkaichi 3.jpg"
-                                nombre="DBZ Budokai Tenkaichi 3"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="./img/PES 2020 3.0 FINAL.jpg"
-                                nombre="PES 2020 3.0 FINAL"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="./img/Naruto Ultimate Ninja 3.jpg"
-                                nombre="Naruto Ultimate Ninja 3"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="https://img.asmedia.epimg.net/resizer/lqsy7mJHPWIbj45aTSL7gwZY4U8=/360x0/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KJSGVNUE3ZJSFLZ5MUJVKHHILY.jpg"
-                                nombre="Grand Theft Auto: San Andreas"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="https://img.asmedia.epimg.net/resizer/lqsy7mJHPWIbj45aTSL7gwZY4U8=/360x0/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KJSGVNUE3ZJSFLZ5MUJVKHHILY.jpg"
-                                nombre="Grand Theft Auto: San Andreas"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="https://img.asmedia.epimg.net/resizer/lqsy7mJHPWIbj45aTSL7gwZY4U8=/360x0/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KJSGVNUE3ZJSFLZ5MUJVKHHILY.jpg"
-                                nombre="Grand Theft Auto: San Andreas"
-                                region="Ntfs-Pal" />
-                        </div>
-                        <div className="carta">
-                            <CaratulaJuego
-                                imagen="https://img.asmedia.epimg.net/resizer/lqsy7mJHPWIbj45aTSL7gwZY4U8=/360x0/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/KJSGVNUE3ZJSFLZ5MUJVKHHILY.jpg"
-                                nombre="Grand Theft Auto: San Andreas"
-                                region="Ntfs-Pal" />
-                        </div>
+                        {
+                            juegos.map((item) => {
+                                return (
+                                    <div key={item.id} className="carta">
+                                        <CaratulaJuego
+                                            imagen={item.imagen}
+                                            nombre={item.nombre}
+                                            region={item.region} />
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
